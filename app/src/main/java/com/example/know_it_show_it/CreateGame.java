3c5a1b3a0317982f7fe4889733e46dd205087c7f
Gevorg Asatryan <72943943+asgevorg@users.noreply.github.com>
2023-02-15 00:51:22 +0400

    Added firestore, creating game with token generating

--- app/src/main/java/com/example/know_it_show_it/CreateGame.java
+++ app/src/main/java/com/example/know_it_show_it/CreateGame.java
@@ -10,14 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.SuccessContinuation;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 
@@ -26,8 +27,7 @@ public class CreateGame extends AppCompatActivity {
     Button create_game_btn;
     EditText game_name;
     Random rand = new Random();
-    private DatabaseReference database;
-    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,20 +35,20 @@ public class CreateGame extends AppCompatActivity {
         setContentView(R.layout.activity_create_game);
         create_game_btn = findViewById(R.id.create_game);
         game_name = (EditText) findViewById(R.id.game_name);
-        database = FirebaseDatabase.getInstance().getReference();
-        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        Map<String, Object> game = new HashMap<>();
+        game.put("Title", game_name.getText().toString());
 
         create_game_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i("","");
+                db.collection("games").document(generate_token()).set(game);
             }
         });
     }
 
     private String generate_token(){
         String game_id = String.format("%04d", rand.nextInt(10000));
-        game_id = "0231";
         return game_id;
     }
 
